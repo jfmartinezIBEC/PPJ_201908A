4d8c02b181c9c5a2998d4b06d0ed7406407f80eb
jfmartinezIBEC <54336211+jfmartinezIBEC@users.noreply.github.com>
2019-09-04 12:49:18 -0300

    Create Clase05-Continue.js

---
+++ Clase05-Continue.js
@@ -0,0 +1,8 @@
+// ejemplo continue (continuar)
+var i=0 
+while (i<7){ 
+   	incrementar = prompt("La cuenta está en " + i + ", dime si incremento", "si") 
+   	if (incrementar == "no") 
+      	continue 
+   	i++ 
+}
